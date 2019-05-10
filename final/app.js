54f52e9f0dec84fe769fbc49ceedca59921676ec
Claire1995 <48541779+Claire1995@users.noreply.github.com>
2019-05-10 12:21:24 +0800

    Add files via upload

---
+++ final/app.js
@@ -0,0 +1,16 @@
+var image1= document.getElementsByClassName('left');
+var image2 = document.getElementsByClassName('right');
+// new simpleParallax(image1, {
+// 	orientation: 'right'
+// 	};)
+// new simpleParallax(image2, {
+// 	orientation: 'left'
+// 	};)
+
+new simpleParallax(image1, {
+	orientation: 'right'
+	});
+new simpleParallax(image2, {
+	orientation: 'left'
+	});
+
